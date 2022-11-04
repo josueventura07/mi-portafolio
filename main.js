b5ac037adb9277da6b5d570c8e73c4165ef0cdb3
josueventura07 <josueandresventura@gmail.com>
2022-11-04 01:16:04 -0400

    actualizacion3

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-const Navbar_hidden = document.querySelector('.navbar_hidden');
+const openMenu = document.querySelector('.bx-menu');
+const navBar = document.querySelector('.navbar');
+const closeMenu = document.querySelector('.fa-xmark');
+const btnDarkMode = document.querySelector('.btn_dark-mode')
+const sliderCircleBottomContainer = document.querySelectorAll('.slider_circle-bottom-container')
+const sliderNext = document.querySelector('.slider_next')
+const sliderImgContainer = document.querySelector('.slider_img-container')
 
+
+
+openMenu.addEventListener('click', (e) => {
+    navBar.classList.toggle('navbar_hidden')
+});
+
+closeMenu.addEventListener('click', (e) => {
+    navBar.classList.toggle('navbar_hidden')
+});
+
+btnDarkMode.addEventListener('click', (e) => {
+    btnDarkMode.classList.toggle('btn_dark-mode-change')
+});
+
+
+
+sliderCircleBottomContainer.forEach((mainbtn, i) => {
+    sliderCircleBottomContainer[i].addEventListener('click', () => {
+        
+        let position = i
+        let operation = position * -50
+
+        sliderImgContainer.style.transform = `traslateX(${operation}%)`
+    })
+});
